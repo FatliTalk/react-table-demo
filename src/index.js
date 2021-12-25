a43bf195e866226c61aca364df66689b186e7f13
Qingquan Li <liqqisme@gmail.com>
2021-12-25 00:49:53 -0500

    Display "Hello World!" with Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
+// Loading the `Component` as a property of React.
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './bootstrap-3.4.1/css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component {
+  render() {
+    return (
+      <div className='jumbotron'>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
